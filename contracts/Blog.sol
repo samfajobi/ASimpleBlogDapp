7b78907849e0cfcf9b1b69d2ede7ccad7c252db2
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-10-26 21:13:25 +0100

    Initial Commit

---
+++ contracts/Blog.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+
+contract Blog {
+    address public owner;
+    uint public activePostsCounter;
+    uint public deletedPostsCounter;
+    uint public totalPostsCounter;
+
+    enum Deactivated { YES, NO }
+
+    struct PostStruct {
+        uint id;
+        address author;
+        string title;
+        string description;
+        uint created;
+        uint updated;
+    }
+
+    mapping(uint => adress) public delPostOf;
+    mapping(uint => address) public authorOf;
+    mapping(address => uint) public postsOf;
+     
+    event
+    modifier onlyOwner() {
+        require(msg.sender = owner, "You are not authorised to make changes");
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+    
+}
+
+
+
+
+// mapping(address => uint) public balances 
+// balances[msg.sender] = 123;  --- (set data)
+// uint bal = balances [msg. sender];  --- (get data)
+// uint bal2 = balances [address (1)]; // 0
+// balances[msg.sender] += 456  --- (update data)
+// delete balances[msg.sender];  --- (delete data)
